6e851ce90ec226d460c4bb03bce29179ba9ee43b
pedro <pedrobastoszo@gmail.com>
2017-10-08 22:53:09 -0300

    hello word no front

---
+++ src/main/java/br/com/nivelrisco/common/MainController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.nivelrisco.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homepage() {
+        return "static/index.html";
+    }
+}
